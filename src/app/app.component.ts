1cfb84fddf6218d96f8576caf5d833f7b3e9d49f
AnhellSteve <dantesteve@hotmail.com>
2023-11-06 20:59:37 -0500

    probando diferentes funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nomangproj001';
+  nombre = '';
+  apellido = '';
+  edad = 20;
+  email = 'DoeJohn@gmail.com';
+  salario = [1700, 1600, 1900];
+  activo = true;
+  sitio = 'https://www.google.com';
+  contador = 1;
+
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador Activo';
+    }else{
+      return 'Trabajador Inactivo';
+    }
+  }
+
+  ultimos3Salario(){
+    let suma = 0;
+    for(let i = 0; i < this.salario.length; i++) {
+      suma+=this.salario[i];
+    }
+    return suma;
+  }
+  
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
 }
